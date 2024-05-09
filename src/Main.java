225a82c1f4619d4544e67c2b7bb6512fa4a99a6f
Gamaliel Quiroz <ghamiboxbox@gmail.com>
2024-05-09 10:32:38 -0300

    Funcionando

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,20 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		JOptionPane.showMessageDialog(null, "Hola espero no se esten durmiendo");
+		Persona catalina = new Persona("Catalina");
+		Persona julieta = new Persona("Julieta");
 		
+		
+		Iphone iphone = new Iphone("2023",catalina,"17.0");
+		Motorola motoU9 = new Motorola("2023",julieta,"7.9");
+		
+		if (iphone.HacerLlamada(17, motoU9)) {
+			JOptionPane.showMessageDialog(null, "El moto le resta" + motoU9.getBateria());
+			JOptionPane.showMessageDialog(null, "El iphone le resta" + iphone.getBateria());
+
+		} else {
+			JOptionPane.showMessageDialog(null, "No se realizo la llamada");
+		}
 		
 	}
 
